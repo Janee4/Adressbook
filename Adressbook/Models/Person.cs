eff8b7077ffd065b48d56c9a6a531c6ab3d79cc9
Jane <hezha.aban@gmail.com>
2023-12-04 19:06:28 +0100

    Add project files.

---
+++ Adressbook/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressbook.Models
+{
+    internal class Person
+    {
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string AdressInformation { get; set; }
+
+    
+    }
+}
+
---
+++ Adressbook/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Adressbook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<string> ContactList = new List<string>();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
