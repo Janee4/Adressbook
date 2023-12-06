85961ae134e60dd916566957abecddceacae5123
Jane <hezha.aban@gmail.com>
2023-12-06 14:26:33 +0100

    sparat in första värdet "FirstName".

--- Adressbook/Services/MenuService.cs
+++ Adressbook/Services/MenuService.cs
@@ -1,40 +1,40 @@
 ﻿
-using Addressbook.Models;
+using Addressbook.Interfaces;
 
 namespace Addressbook.Models;
 
 
 
-    //skapa en Interface som hanterar utskriften av menyn, den behöver inte returnera något så därav "void"
-   public interface IMenuService
+//skapa en Interface som hanterar utskriften av menyn, den behöver inte returnera något så därav "void"
+public interface IMenuService
+{
+    //skapa en metod som sköter utskriften av menyn
+    void OptionsMenu();
+
+}
+
+
+
+//Denna klassen innehåller inga properties utan endast logiken för att utföra dessa metoder som den innehåller
+public class MenuService : IMenuService
+{
+    public void OptionsMenu()
     {
-        //skapa en metod som sköter utskriften av menyn
-        void OptionsMenu();
-        
-    }
-
-
-
-    //Denna klassen innehåller inga properties utan endast logiken för att utföra dessa metoder som den innehåller
-    public class MenuService : IMenuService
-    {
-        public void OptionsMenu()
+        while (true)
         {
-            while (true) 
-            {
-                Console.Clear();
+            Console.Clear();
             DisplayMenuTitle("MENU OPTIONS");
             Console.WriteLine(
                 "\n1: Add Contact To The Addressbook" +
                 "\n2: Remove Contact From The Addressbook" +
                 "\n3: Show All Contacts In The Addressbook" +
                 "\n4: Exit Program");
-                Console.Write("");
-                Console.Write("Enter Menu Option: ");
+            Console.Write("");
+            Console.Write("Enter Menu Option: ");
             //användarens svar sparas i variabeln "option" och används sedan i switchen för att välja menyval
             var option = Console.ReadLine();
 
-            switch(option)
+            switch (option)
             {
 
                 case "1":
@@ -45,10 +45,10 @@ namespace Addressbook.Models;
                     RemoveContactOption();
                     break;
 
-               case "3":
+                case "3":
                     AllContactsOption();
                     break;
-               
+
                 case "4":
                     ExitApplicationOption();
                     break;
@@ -59,36 +59,37 @@ namespace Addressbook.Models;
                     break;
 
             }
-            
+
         }
     }
 
-        //Dessa metoder sätts som private för att de endast behöver nås ifrån MenuService klassen då bara den här klassen ska styra vilken meny som ska visas.
-        private void AddContactOption()
-        {
+    //Dessa metoder sätts som private för att de endast behöver nås ifrån MenuService klassen då bara den här klassen ska styra vilken meny som ska visas.
+    private void AddContactOption()
+    {
 
-        //Instansiera en ny contact
+        //Instansiera en ny contact från klassen contact?
         IContact contact = new Contact();
         DisplayMenuTitle("Add New Contact");
         Console.WriteLine("First Name: ");
+        //Spara in användarens värde av "FirstName" och lagra in den i "contact.FirstName" vilket kommer att spara in värdet i vår Contact klass (vi har ju properties där och användarens svar sparas in i dessa properties).
+        contact.FirstName = Console.ReadLine()!; //Med "!" lovar vi kompilatorn att jag som utvecklare tar på mig ansvaret att garantera att värdet inte är 'null', dvs det kommer att komma in ett svar som inte är null.
+    }
 
-        }
+    private void AllContactsOption()
+    {
 
-        private void AllContactsOption()
-        {
-            
-        }
+    }
 
-        private void RemoveContactOption()
-        {
-            
-        }
+    private void RemoveContactOption()
+    {
 
-        private void ExitApplicationOption() 
-        {
+    }
+
+    private void ExitApplicationOption()
+    {
         Console.Clear();
         Console.Write("Are you sure you want to exit? (y/n): ");
-       //Med "?? "" " så säger vi att om användaren inte skriver in något så får vi en tom sträng 
+        //Med "?? "" " så säger vi att om användaren inte skriver in något så får vi en tom sträng 
         var input = Console.ReadLine() ?? " ";
         //Oavsett om användaren skriver in Y med stor bokstav eller y med liten, så kommer programmet att stängas av.
         if (input.Equals("y", StringComparison.CurrentCultureIgnoreCase))
@@ -100,17 +101,14 @@ namespace Addressbook.Models;
 
 
 
-        }
-        //Skapa en metod som skriver ut titeln på menyn som du kan återanvända för varje option och lägga till valfri titel
-        private void DisplayMenuTitle (string title)
-        {
+    }
+    //Skapa en metod som skriver ut titeln på menyn som du kan återanvända för varje option och lägga till valfri titel
+    private void DisplayMenuTitle(string title)
+    {
         Console.Clear();
-        Console.Write ($"### {title} ###");
-        Console.WriteLine ();
-        }
-
+        Console.Write($"### {title} ###");
+        Console.WriteLine();
     }
 
-internal interface IContact
-{
 }
+
